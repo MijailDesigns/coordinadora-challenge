f74719af205d8bbc73a8baaaf01fee9397e18381
mijail pulgar <mpulgar@abatechnology.com>
2025-02-14 16:04:44 -0500

    feat: swagger documentation for driver and truck dto

--- src/drivers/dto/create-driver.dto.ts
+++ src/drivers/dto/create-driver.dto.ts
@@ -1,15 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateDriverDto {
   @IsNotEmpty({ message: 'La placa no puede estar vacia' })
   @IsString({ message: 'La placa debe ser una cadena de texto' })
+  @ApiProperty({
+    description: 'Driver name',
+    required: true,
+    type: String,
+  })
   nombre: string;
 
   @IsNotEmpty({ message: 'El modelo no puede estar vacio' })
   @IsString({ message: 'El modelo debe ser una cadena de texto' })
+  @ApiProperty({
+    description: 'License driver',
+    required: true,
+    type: String,
+  })
   licencia: string;
 
   @IsNumber()
   @IsOptional()
+  @ApiProperty({
+    description: 'Truck id',
+    required: false,
+    type: Number,
+  })
   truckId: number;
 }
--- src/trucks/dto/create-truck.dto.ts
+++ src/trucks/dto/create-truck.dto.ts
@@ -1,15 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateTruckDto {
   @IsNotEmpty({ message: 'La placa no puede estar vacia' })
   @IsString({ message: 'La placa debe ser una cadena de texto' })
+  @ApiProperty({
+    description: 'License plate',
+    required: true,
+    type: String,
+  })
   placa: string;
 
   @IsNotEmpty({ message: 'El modelo no puede estar vacio' })
   @IsString({ message: 'El modelo debe ser una cadena de texto' })
+  @ApiProperty({
+    description: 'Truck model',
+    required: true,
+    type: String,
+  })
   modelo: string;
 
   @IsNumber()
   @IsOptional()
-  driverId: number;
+  @ApiProperty({
+    description: 'Driver id',
+    required: false,
+    type: Number,
+  })
+  driverId?: number;
 }
